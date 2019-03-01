b97cf0d64408e35dde62b64b450f9e875d3b111e
Nataly <nataly05olivares@gmail.com>
2019-03-01 06:37:26 +0000

    SAVED

--- js/game.js
+++ js/game.js
@@ -21,19 +21,25 @@ var game = {
         },
 
         help: {
-            background_image: "fire.gif",
+            background_image: "https://i.pinimg.com/originals/a4/45/b8/a445b8cd3f0adc4a06580c4b2fc00ff5.jpg",
+
             music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "",
+            message: "You explain them your situtation and ask them for their help. They agreed to help you but in return you... ",
             choices: [
                 {
-                    text: "Start over",
+                    text: "Buy them a beer",
                     nextLevel: "start",
+                },
+                
+                {
+                    text: "Buy them some pie",
+                    nextLevel:"start",
                 },
             ]
         },
 
         alone: {
-            message: "You were killed by a Windego because you have no idea how to kill it.",
+            message: "A few days later, you were killed by a Wendigo because you have no idea how to kill it, loser.",
             choices: [
                 {
                     text: "Start over",
